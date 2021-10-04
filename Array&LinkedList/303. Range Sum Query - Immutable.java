9fcb4ca4f9bc58347fde8bbc7b081d6c843f021e
Thinh Nguyen <ngtrphuocthinh@gmail.com>
2021-10-04 12:56:26 +0700

    first 2 problems

---
+++ Array&LinkedList/303. Range Sum Query - Immutable.java
@@ -0,0 +1,23 @@
+class NumArray {
+
+    int[] sums;
+
+    public NumArray(int[] nums) {
+        sums = new int[nums.length];
+        sums[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sums[i] = sums[i - 1] + nums[i];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        if (left == 0)
+            return sums[right];
+        return sums[right] - sums[left - 1];
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such: NumArray obj =
+ * new NumArray(nums); int param_1 = obj.sumRange(left,right);
+ */
