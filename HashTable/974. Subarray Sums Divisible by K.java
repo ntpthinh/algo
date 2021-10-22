e0a6922884f34fba4f33f53e7906e63d787aa6c6
Thinh Nguyen <ngtrphuocthinh@gmail.com>
2021-10-22 22:56:54 +0700

    description for sub array sum divisible by k

--- HashTable/974. Subarray Sums Divisible by K.java
+++ HashTable/974. Subarray Sums Divisible by K.java
@@ -5,7 +5,12 @@ class Solution {
         /**
          * general idea: use map to store all remainders and its frequency of the sum of
          * elements up to current index. If the remainder already existed in the map, it
-         * means that the sum is divided 
+         * means that there is at least one sum that have the same remainder as the
+         * current element. We might think of this as: sum1 = n*k + r1, sum2 = m*k + r2,
+         * if r1 == r2, sum2 - sum1 = (m-n)*k => there is a subarray that sum is
+         * divisible by k. To count total number of subarray, we need to keep the
+         * frequency of that remainder prior to current element If remainder < 0,
+         * remainder += k
          */
         Map<Integer, Integer> map = new HashMap();
         map.put(0, 1);
