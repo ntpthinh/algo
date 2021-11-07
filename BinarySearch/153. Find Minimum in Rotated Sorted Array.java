be0bc8705b5b1151d74164da555e7e675e8e9c0c
Thinh Nguyen <ngtrphuocthinh@gmail.com>
2021-11-07 11:52:50 +0700

    bin search

--- BinarySearch/153. Find Minimum in Rotated Sorted Array.java
+++ BinarySearch/153. Find Minimum in Rotated Sorted Array.java
@@ -15,3 +15,23 @@ class Solution {
         return nums[start];
     }
 }
+
+class Solution {
+    public int findMin(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            if (nums[start] <= nums[end])
+                return nums[start];
+            int mid = start + (end - start) / 2;
+            if (mid > 0 && nums[mid] < nums[mid - 1])
+                return nums[mid];
+            if (nums[mid] < nums[start])
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        return -1;
+    }
+}
---
+++ BinarySearch/81. Search in Rotated Sorted Array II.java
@@ -0,0 +1,3 @@
+public class 81. Search in Rotated Sorted Array II {
+    
+}
